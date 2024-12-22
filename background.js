8bf192071f520badb19681f5bd3cb7bb696d70f9
Francis Y. Yan <francisyyan@gmail.com>
2024-12-21 21:30:34 -0800

    Decode Proofpoint URL Defense urls as well

--- background.js
+++ background.js
@@ -1,30 +1,46 @@
-function convert_safelinks() {
+function decode_urls() {
+  const urldefense_prefix = /^https:\/\/urldefense\.com\/v3\/__(.+?)__;(.*?)!/;
   const safelinks_prefix = /^https:\/\/[a-z0-9]+\.safelinks\.protection\.outlook\.com\/\?url=(.+?)&/;
 
   var urls = document.getElementsByTagName('a');
 
   for (var i = 0; i < urls.length; i++) {
-    // only convert the URLs that appear as Safe Links
     const url = urls[i].innerText;
 
-    const result = url.match(safelinks_prefix);
-    if (result === null || result.length !== 2) {
+    // match URL Defense urls
+    var matches = url.match(urldefense_prefix);
+    if (matches && matches.length == 3) {
+      // adapted from https://gist.github.com/baldwicc/3623b8eea96eb0cbbe41b1f51aeca21f
+      var decode_pile = Array.from(matches[1]);
+      var chars_pile = Array.from(atob(matches[2])).reverse();
+
+      for (var match of url.matchAll(/\*/g)) {
+        decode_pile[match.index] = chars_pile.pop();
+      }
+
+      urls[i].innerText = decode_pile.join('');
       continue;
     }
 
-    try {
-      const decoded_url = decodeURIComponent(result[1]);
-      urls[i].innerText = decoded_url;
-    } catch (e) {
-      console.error(e);
+    // match Safe Links urls
+    matches = url.match(safelinks_prefix);
+    if (matches && matches.length == 2) {
+      try {
+        const decoded_url = decodeURIComponent(matches[1]);
+        urls[i].innerText = decoded_url;
+      } catch (e) {
+        console.error(e);
+      }
+
+      continue;
     }
   }
 }
 
-// run function 'convert_safelinks' once the extension's icon is clicked
+// run function 'decode_urls' once the extension's icon is clicked
 chrome.action.onClicked.addListener(function(tab) {
   chrome.scripting.executeScript({
     target: {tabId: tab.id},
-    function: convert_safelinks
+    function: decode_urls
   });
 });
